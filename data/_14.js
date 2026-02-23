3c5b14053ed16498770ba7749639e13eec77bdcb
masa2891 <61530960+masa2891@users.noreply.github.com>
2026-02-23 13:08:00 +0900

    Add files via upload

---
+++ data/_14.js
@@ -0,0 +1,9 @@
+var json__14 = {
+"type": "FeatureCollection",
+"name": "_14",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "種類": "施設", "番号": 53.0, "名称": "SUP（※桜淵公園内の豊川）", "住所（新城市HP・愛知県公式ガイド・キラッと奥三河観光ナビに記載住所等）": "新城市滝ノ上49", "Googlemapで検索すると表示される住所等": null, "湯谷温泉駅からの車での移動時間": "23分", "その他特徴": null, "LocName": "愛知県/新城市/滝ノ上", "fX": 137.50331, "fY": 34.89524, "iConf": 5.0, "iLvl": 5.0 }, "geometry": { "type": "Point", "coordinates": [ 137.503313983424277, 34.89523817156612 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "種類": "施設", "番号": 62.0, "名称": "フォレストアドベンチャー新城", "住所（新城市HP・愛知県公式ガイド・キラッと奥三河観光ナビに記載住所等）": "新城市大海谷下66?１ 新城総合公園内", "Googlemapで検索すると表示される住所等": null, "湯谷温泉駅からの車での移動時間": "17分", "その他特徴": null, "LocName": "愛知県/新城市/大海", "fX": 137.54571, "fY": 34.93556, "iConf": null, "iLvl": null }, "geometry": { "type": "Point", "coordinates": [ 137.54571106233513, 34.935561625247686 ] } }
+]
+}
---
+++ data/_17.js
@@ -0,0 +1,16 @@
+var json__17 = {
+"type": "FeatureCollection",
+"name": "_17",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "種類": "旅館", "番号": "57", "名称": "湯の風 HAZU", "住所（新城": "新城市能登瀬上谷平4-1", "Googlemap": null, "湯谷温泉駅": "2分", "その他特徴": null, "LocName": "愛知県/新城市/能登瀬", "fX": 137.61435, "fY": 34.96816, "iConf": "5", "iLvl": "5" }, "geometry": { "type": "Point", "coordinates": [ 137.614345007801774, 34.968161471776149 ] } },
+{ "type": "Feature", "properties": { "種類": "旅館", "番号": "58", "名称": "湯谷観光ホテル泉山閣（廃業）", "住所（新城": "新城市豊岡字滝上26　", "Googlemap": null, "湯谷温泉駅": "1分", "その他特徴": "廃業", "LocName": "愛知県/新城市/豊岡", "fX": 137.61461, "fY": 34.97259, "iConf": "5", "iLvl": "5" }, "geometry": { "type": "Point", "coordinates": [ 137.614610882813736, 34.972585819248565 ] } },
+{ "type": "Feature", "properties": { "種類": "旅館", "番号": "59", "名称": "はづ木", "住所（新城": "新城市豊岡滝上45 1", "Googlemap": null, "湯谷温泉駅": "1分", "その他特徴": null, "LocName": "愛知県/新城市/豊岡", "fX": 137.61396, "fY": 34.97088, "iConf": "5", "iLvl": "5" }, "geometry": { "type": "Point", "coordinates": [ 137.61396031712502, 34.970882340774814 ] } },
+{ "type": "Feature", "properties": { "種類": "旅館", "番号": "60", "名称": "旅荘 みつい", "住所（新城": "新城市豊岡字大谷下35-1", "Googlemap": null, "湯谷温泉駅": "1分", "その他特徴": null, "LocName": "愛知県/新城市/豊岡", "fX": 137.6144, "fY": 34.97202, "iConf": "5", "iLvl": "5" }, "geometry": { "type": "Point", "coordinates": [ 137.614402789240557, 34.972018682387024 ] } },
+{ "type": "Feature", "properties": { "種類": "旅館", "番号": "61", "名称": "旅館　ひさご", "住所（新城": "新城市豊岡字大谷下46-2", "Googlemap": null, "湯谷温泉駅": "1分", "その他特徴": null, "LocName": "愛知県/新城市/豊岡", "fX": 137.61448, "fY": 34.97222, "iConf": "5", "iLvl": "5" }, "geometry": { "type": "Point", "coordinates": [ 137.614477474671503, 34.972219607379984 ] } },
+{ "type": "Feature", "properties": { "種類": "旅館", "番号": "62", "名称": "翆明", "住所（新城": "新城市能登瀬字上谷平6-2", "Googlemap": null, "湯谷温泉駅": "4分", "その他特徴": null, "LocName": "愛知県/新城市/能登瀬", "fX": 137.6141, "fY": 34.96937, "iConf": "5", "iLvl": "5" }, "geometry": { "type": "Point", "coordinates": [ 137.614099261456005, 34.969367416943768 ] } },
+{ "type": "Feature", "properties": { "種類": "旅館", "番号": "63", "名称": "はづ別館", "住所（新城": "新城市豊岡字滝上11-4", "Googlemap": null, "湯谷温泉駅": "1分", "その他特徴": null, "LocName": "愛知県/新城市/豊岡", "fX": 137.61375, "fY": 34.97031, "iConf": "5", "iLvl": "5" }, "geometry": { "type": "Point", "coordinates": [ 137.613746946726422, 34.97030981341846 ] } },
+{ "type": "Feature", "properties": { "種類": "旅館", "番号": "64", "名称": "Hoo!Hoo!", "住所（新城": "新城市豊岡字滝上31-1", "Googlemap": null, "湯谷温泉駅": "1分", "その他特徴": null, "LocName": "愛知県/新城市/豊岡", "fX": 137.61391, "fY": 34.97064, "iConf": "5", "iLvl": "5" }, "geometry": { "type": "Point", "coordinates": [ 137.613905098675474, 34.970635911362486 ] } },
+{ "type": "Feature", "properties": { "種類": "旅館", "番号": "65", "名称": "はづ合掌", "住所（新城": "新城市豊岡字南平18-1", "Googlemap": null, "湯谷温泉駅": "5分", "その他特徴": null, "LocName": "愛知県/新城市/豊岡", "fX": 137.6156, "fY": 34.98943, "iConf": "5", "iLvl": "5" }, "geometry": { "type": "Point", "coordinates": [ 137.615601668892452, 34.989426717341402 ] } }
+]
+}
